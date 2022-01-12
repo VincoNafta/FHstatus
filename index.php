ad642e7236faa33232fe725fc32889b6bff6053b
VincoNafta <62835475+VincoNafta@users.noreply.github.com>
2022-01-13 00:33:31 +0100

    Update 2.0
    Pridané ďaľšie možnosti, odteraz tento status nemusíte nutne používať ako minecraft status, tento status je možné použiť aj pre iné hry od spoločnosti FakaHeda.
    V tejto aktualizácii pribudli veci týkajúce sa hlavne iných hier, teda ako a čo zobraziť ak sa nezobrazí minecraft skin, teda môžte si zobraziť špeciálny znak pred menom alebo aj hocijaký iný znak, prípadne medzeru alebo nemusíte zobraziť nič

--- index.php
+++ index.php
@@ -1,18 +1,24 @@
 <?php
 //IP adresa vášho servera(zvyčajne sa začína 82.208)
-$ip= '82.208.17.32';
+$ip = '82.208.17.32';
 //Port nájdete za dvojbockou
-$port= 27548;
+$port = 27548;
 //V prípade že nemáte domenu, prepíšte IP  a port v tvare XXXX:XXX
 $dns = 'mojadomena.sk';
 //zobrazenie DNS
 $zobrazdns = true;
+//Zobrazenie WEBU
+$zobrazWeb = true;
+//Definovanie vašej webstranky
+$web = "môjkranyweb.sk";
 //Zobrazenie statusu
 $zobrazstatus = true;
 //nastavenie zobrazenie počtu hráčov
 $zobrazhracov = true;
 //zobrazenie verzie
 $zobrazverziu = true;
+//Zobrazenie mapy
+$zobrazMapu = true;
 //zobrazenie využitia procesora
 $zobrazcpu = true;
 //zobrazenie využitia RAM
@@ -20,28 +26,44 @@ $zobrazpamat = false;
 //Zobrazenie listu
 $zobrazlist = true;
 //Spôsob zobrazenia listu(tabuľka alebo riadok)
-$zobrazenie = 'tabulka';
+$zobrazenie = 'riadok';
+//Zobraziť skin
+$zobrazSkin = false;
+//špecialny znak pre zobrazenie v prípade že si neželáte použiť skin. V prípade, že nechcete zobraziť nič, tak nechajte túto syntax ""
+$specialZnak = "○";
 //velkosť avataru v pixeloch
 $velkostav = 16;
+
+
+
+
+
 //Pokiaľ kód funguje do tejto časti nezasahujte, teda pokiaľ neviete čo presne robíte
 $zdroj = json_decode(file_get_contents("https://query.fakaheda.eu/".$ip.":".$port.".feed"));
 
 $status = $zdroj->status;
 $players = $zdroj->players;
 $maxplayers = $zdroj->slots;
+$map = $zdroj->map;
 $version = $zdroj->version;
 $cpu = $zdroj->cpu;
 $memory = $zdroj->memory;
 $list = $zdroj->players_list;
 
+if($zobrazstatus == true){
+	echo "<br>Status: " .$status;
+}
 if ($zobrazdns == true) {
 	echo "<br>IP: ".$dns;
 }
-if($zobrazstatus == true){
-	echo "<br>Status:" .$status;
+if ($zobrazWeb == true) {
+	echo "<br>WEB: ".$web;
 }
 if ($zobrazhracov == true) {
 	echo "<br>Hraci: ".$players."/".$maxplayers;
+}
+if ($zobrazMapu == true) {
+	echo "<br>Mapa: ".$map;
 }
 if($zobrazverziu == true){
 	echo "<br>Verzia: ". $version;
@@ -57,14 +79,22 @@ if ($zobrazlist == true) {
 	if($zobrazenie == 'riadok'){
 		foreach ($list as $key => $value) {
 			$hrac = $value->name;
-			echo "<img src='https://minotar.net/avatar/".$hrac."' height=".$velkostav."'>".$hrac." ";		
+			if ($zobrazSkin == true) {
+				echo "<img src='https://minotar.net/avatar/".$hrac."' height=".$velkostav."'>".$hrac." ";		
+			} else {
+				echo " ".$specialZnak. " " .$hrac;
+			}
 		}
 	}
 	else{
 	echo("<table>");
 		foreach ($list as $key => $value) {
 			$hrac = $value->name;
-			echo "<tr><td><img src='https://minotar.net/avatar/".$hrac."' height=".$velkostav."'> ".$hrac." </td></tr>";		
+			if ($zobrazSkin == true) {
+				echo "<tr><td><img src='https://minotar.net/avatar/".$hrac."' height=".$velkostav."'> ".$hrac." </td></tr>";		
+			} else {
+				echo "<tr><td> ".$specialZnak. " " .$hrac . "</tr></td>";
+			}
 		}
 		echo "</table>";
 	}
